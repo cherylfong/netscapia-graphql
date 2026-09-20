f7dd4373bce1de3f7c9e9cfff8ba9fadce77902b
cherylfong <42821602+cherylfong@users.noreply.github.com>
2026-09-20 17:42:49 +0000

    add guided code that handles making queries, caching, updating cache, and mutations

---
+++ phonebook/client/src/App.jsx
@@ -0,0 +1,22 @@
+import { useQuery } from '@apollo/client/react'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+import { ALL_PERSONS } from './queries'
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  return (
+    <div>
+        <h1>HELLO</h1>
+      <Persons persons={result.data.allPersons} />
+      <PersonForm />
+    </div>
+  )
+}
+
+export default App

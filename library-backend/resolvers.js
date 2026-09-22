3a1c4c136a37daeee219146d682c4eab39dce06f
cherylfong <42821602+cherylfong@users.noreply.github.com>
2026-09-22 23:57:25 +0000

    complete exercises 13 and 14

--- library-backend/resolvers.js
+++ library-backend/resolvers.js
@@ -1,57 +1,102 @@
 const { v4: uuidv4 } = require('uuid')
+const Author = require('./models/author')
+const Book = require('./models/book')
 
-const countBooksByAuthor = (authorName) =>
-  books.filter((book) => book.author === authorName).length
+const { GraphQLError } = require('graphql')
 
-const booksByAuthor = (authorName) =>
-  books.filter((book) => book.author === authorName)
-
-const booksByGenre = (genre) =>
-  books.filter((book) => book.genres.includes(genre))
-
-const authorByName = (authorName) => authors.find((a) => a.name === authorName)
+const countBooksByAuthor = (authorId) =>
+  Book.countDocuments({ author: authorId }).exec()
+//   books.filter((book) => book.author === authorName).length
 
 const resolvers = {
   Query: {
-    authorCount: () => authors.length,
-    bookCount: (root, args) =>
-      args.name ? countBooksByAuthor(args.name) : books.length,
-    allBooks: (root, args) => {
-      if (args.genre) {
-        return booksByGenre(args.genre)
+    authorCount: async () => Author.collection.countDocuments(),
+
+    allAuthors: async (root, args) => Author.find({}).exec(),
+
+    bookCount: async (root, args) => {
+      if (!args.name) {
+        return Book.countDocuments({}).exec()
       }
 
-      return args.author ? booksByAuthor(args.author) : books
+      const author = await Author.findOne({ name: args.name }).exec()
+
+      return author ? countBooksByAuthor(author._id) : 0
     },
-    allAuthors: () => authors,
+
+    allBooks: async (root, { author, genre }) => {
+      const filter = {}
+
+      if (author) filter.author = author
+      if (genre) filter.genres = { $all: [genre] }
+
+      // it is necessary to populate the field 'author' in the Book schema!
+      // exec() returns a promise
+      //
+      // .exec() means: “Run this Mongoose query.”
+      // await means: “Pause here until the promise finishes.”
+      // Mongoose queries are thenable, so this often also works:
+      // const books = await Book.find({})
+      // But .exec() makes the query execution explicit and returns a standard promise.
+      return Book.find(filter).populate('author').exec()
+    },
   },
 
   // self-defined resolver
   Author: {
     // receives parent author object as param
-    bookCount: (author) => countBooksByAuthor(author.name),
+    bookCount: (author) => countBooksByAuthor(author._id),
   },
 
   Mutation: {
-    addBook: (root, args) => {
-      if (!authorByName(args.author)) {
-        //if author does not exist then add to authors
-        const author = { name: args.author, born: null, id: uuidv4() }
-        authors = authors.concat(author)
+    addBook: async (root, args) => {
+      let author = await Author.findOne({ name: args.author })
+
+      if (!author) {
+        author = await new Author({ name: args.author }).save()
       }
-      const book = { ...args, id: uuidv4() }
-      books = books.concat(book)
+
+      const book = new Book({
+        title: args.title,
+        author: author,
+        published: args.published,
+        genres: args.genres,
+      })
+
+      try {
+        await book.save()
+        await book.populate('author')
+      } catch (error) {
+        throw new GraphQLError(`Saving BOOK failed: ${error.message}`, {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.title,
+            error,
+          },
+        })
+      }
+
       return book
     },
-    editAuthor: (root, args) => {
-      const author = authorByName(args.name)
+    editAuthor: async (root, args) => {
+      const author = await Author.findOne({ name: args.name })
       if (!author) {
         return null
       }
+      author.born = args?.setBornTo
 
-      const updatedAuthor = { ...author, born: args.setBornTo }
-      authors = authors.map((a) => (a.name === args.name ? updatedAuthor : a))
-      return updatedAuthor
+      try {
+        await author.save()
+      } catch (error) {
+        throw new GraphQLError(`Failed to UPDATE AUTHOR: ${error.message}`, {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+            error,
+          },
+        })
+      }
+      return author
     },
   },
 }
--- library-backend/schema.js
+++ library-backend/schema.js
@@ -1,4 +1,3 @@
-
 const typeDefs = /* GraphQL */ `
   type Author {
     name: String!

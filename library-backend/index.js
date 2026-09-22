ba4e4533b9725a2b8083c93289a51e66a873efc7
cherylfong <42821602+cherylfong@users.noreply.github.com>
2026-09-22 21:15:17 +0000

    move typeDefs into schema module

--- library-backend/index.js
+++ library-backend/index.js
@@ -94,43 +94,7 @@ let books = [
   },
 ]
 
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
 
-const typeDefs = /* GraphQL */ `
-  type Author {
-    name: String!
-    id: ID!
-    born: Int
-    bookCount: Int!
-  }
-
-  type Book {
-    title: String!
-    published: Int!
-    author: String!
-    id: ID!
-    genres: [String!]!
-  }
-
-  type Query {
-    authorCount: Int!
-    bookCount(name: String): Int!
-    allBooks(author: String, genre: String): [Book!]!
-    allAuthors: [Author!]!
-  }
-
-  type Mutation {
-    addBook(
-      title: String!
-      author: String!
-      published: Int!
-      genres: [String!]!
-    ): Book
-    editAuthor(name: String!, setBornTo: Int!): Author
-  }
-`
 
 const countBooksByAuthor = (authorName) =>
   books.filter((book) => book.author === authorName).length
